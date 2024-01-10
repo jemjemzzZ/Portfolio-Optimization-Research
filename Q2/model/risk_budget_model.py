32368a325c0ed8f0259e81bac971131cb50116d2
Jiawen Wen <jiawen0105@outlook.com>
2024-01-10 20:49:33 +1100

    update Q2

--- Q2/model/risk_budget_model.py
+++ Q2/model/risk_budget_model.py
@@ -3,20 +3,26 @@ from .tool import get_risk_budget
 
 
 class RBModel:
-    def __init__(self, tmp_close, weight_constraints, actual_data):
+    def __init__(self, tmp_close, weight_constraints, actual_data, risk_alloc_method=0):
         self.tmp_close = tmp_close
         self.weight_constraints = weight_constraints
         self.actual_data = actual_data
+        self.risk_alloc_method = 0
 
     def optimize(self):
         S = (self.tmp_close.pct_change().dropna()).cov() * 252
-        risk_alloc = self.get_risk()
+        risk_alloc = self.get_risk(self.risk_alloc_method)
         
         new_weight = get_risk_budget(cov=S, risk_alloc=risk_alloc, bounds=self.weight_constraints)
+        print(new_weight)
         return new_weight
     
-    def get_risk(self):
-        daily_returns = self.actual_data.pct_change().dropna()
+    def get_risk(self, risk_alloc_method):
+        if risk_alloc_method == 1:
+            risk_alloc_data = self.tmp_close
+        else:
+            risk_alloc_data = self.actual_data
+        daily_returns = risk_alloc_data.pct_change().dropna()
         volatility = daily_returns.std()
         normalized_risk = volatility / volatility.sum()
         return normalized_risk
--- Q2/tool.py
+++ Q2/tool.py
@@ -23,8 +23,10 @@ def evaluate(historical_data,
     if model_type == 'BL':
         target_method = 'min_volatility'
         model = BLModel(historical_data, target_method, target_return, weight_constraints, risk_free_rate, future_data)
-    if model_type == 'RB':
-        model = RBModel(historical_data, weight_constraints, future_data)
+    if model_type == 'RB': # use future data to calculate risk allocation
+        model = RBModel(historical_data, weight_constraints, future_data, risk_alloc_method=0)
+    if model_type == 'RB-H': # use historical data to calculate risk allocation
+        model = RBModel(historical_data, weight_constraints, future_data, risk_alloc_method=1)
     
     try:
         new_weight = model.optimize()
